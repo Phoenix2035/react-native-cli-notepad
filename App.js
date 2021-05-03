49ab93d413c7da81dbc1de31405f09b151c92dcb
Ali Gh <aligodosi@gmail.com>
2021-05-03 21:56:49 +0430

    init

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    Text,
+    View
+} from 'react-native';
+
+
+const App = () => {
+    return (
+        <View>
+            <Text>
+                Hello World 5
+            </Text>
+
+        </View>
+    );
+};
+
+
+export default App;
---
+++ app/components/Layout.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container,
+  Header, Footer, FooterTab, Left, Right, Body, Title } from "native-base"
+
+
+function Layout({ children, left, right, title, footer }) {
+    return (
+        <Container>
+            <Header>
+                <Left>
+                    {left}
+                </Left>
+                <Body>
+                    <Title>
+                        {title}
+                    </Title>
+                </Body>
+                <Right>
+                    {right}
+                </Right>
+            </Header>
+            {children}
+            <Footer>
+                <FooterTab>
+                    {footer}
+                </FooterTab>
+            </Footer>
+        </Container>
+    )
+}
+
+export default Layout;
